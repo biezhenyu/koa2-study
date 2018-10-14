7e93ee2a10cda25e9ba95d40c9aa8df26b1344d8
biezhenyu <biezhenyu521@163.com>
2018-10-14 15:35:41 +0800

    1.2 async/awit简单使用

---
+++ server/async.js
@@ -0,0 +1,22 @@
+function getSyncTime(params) {
+  return new Promise((resolve, reject) => {
+    try {
+      let startTime = new Date().getTime()
+      setTimeout(() => {
+        let endTime = new Date().getTime()
+        let data = endTime - startTime
+        resolve( data )
+      }, 500)
+    } catch ( err ) {
+      reject( err )
+    }
+  });
+}
+
+async function getSyncData() {
+  let time = await getSyncTime()
+  let data = `endTime - startTime = ${time}`
+  console.log(data)
+}
+
+getSyncData()
