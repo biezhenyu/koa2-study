dab4001093497d0954824aa812ed17aa34fcea71
biezhenyu <biezhenyu521@163.com>
2018-10-18 23:22:20 +0800

    3.1 koa get请求

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## koa2 学习
 
 笔记放在note
+
+node自动重启 supervisor
---
+++ server/get.js
@@ -0,0 +1,29 @@
+const koa = require('koa');
+const app = new koa();
+app.use(async ctx => {
+  let url = ctx.url;   // url路径
+
+  // 从上下文的request对象中获取
+  let request = ctx.request
+  let req_query = request.query
+  let req_querystring = request.querystring
+
+  // 从上下文中直接获取
+  let ctx_query = ctx.query
+  let ctx_querystring = ctx.querystring
+
+
+  ctx.body = {
+    url,
+    request,
+    req_query,
+    req_querystring,
+    ctx_query,
+    ctx_querystring
+  }
+})
+
+
+app.listen(3000, () => {
+  console.log('[demo] request get is starting at port 3000')
+})
